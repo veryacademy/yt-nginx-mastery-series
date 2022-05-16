b3228133617d4538e2ab96bff869c72c90b2251e
Zander <azander610153@gmail.com>
2022-05-16 14:47:59 +0100

    nc

---
+++ part-7-nginx-split-test/django2/demo/urls.py
@@ -0,0 +1,8 @@
+from django.contrib import admin
+from django.urls import path
+from django.views.generic import TemplateView
+
+urlpatterns = [
+    path('admin/', admin.site.urls),
+    path('split-test/', TemplateView.as_view(template_name="index.html")),
+]
